58cd20bfd3904e08d02136e455313598dd0dd0c2
Jiang Zheyi <jiangzheyi1234@gmail.com>
2026-03-23 14:57:54 +0800

    refactor(ui): split main window coordination into dedicated controllers

--- ui/controllers/__init__.py
+++ ui/controllers/__init__.py
@@ -1,5 +1,13 @@
 """UI controllers."""
 
 from ui.controllers.home_page_controller import HomePageController
+from ui.controllers.main_window_disk_monitor import MainWindowDiskMonitor
+from ui.controllers.main_window_log_controller import MainWindowLogController
+from ui.controllers.main_window_project_controller import MainWindowProjectController
 
-__all__ = ["HomePageController"]
+__all__ = [
+    "HomePageController",
+    "MainWindowDiskMonitor",
+    "MainWindowLogController",
+    "MainWindowProjectController",
+]
