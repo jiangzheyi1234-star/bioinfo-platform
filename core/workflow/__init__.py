b5310aae13affc9c4a7cc159838a0ebf8a017d40
Jiang Zheyi <jiangzheyi1234@gmail.com>
2026-04-14 01:37:14 +0800

    Add single-node workflow launcher loop

--- core/workflow/__init__.py
+++ core/workflow/__init__.py
@@ -2,6 +2,7 @@
 
 from .compiler import compile_workflow_bundle
 from .domain import LaunchSpec, RunRecord, ServerProfile, ToolSpec, WorkflowEdge, WorkflowNode, WorkflowSpec
+from .runtime_ops import download_run_artifacts, materialize_bundle, persist_run_record, query_local_nextflow_run, recursive_upload_directory, submit_local_nextflow_run
 
 __all__ = [
     "LaunchSpec",
@@ -12,4 +13,10 @@ __all__ = [
     "WorkflowNode",
     "WorkflowSpec",
     "compile_workflow_bundle",
+    "download_run_artifacts",
+    "materialize_bundle",
+    "persist_run_record",
+    "query_local_nextflow_run",
+    "recursive_upload_directory",
+    "submit_local_nextflow_run",
 ]
