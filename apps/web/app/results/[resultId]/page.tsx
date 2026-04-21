52c76d7277bd78b1326ad7049568fe82471edefd
Jiang Zheyi <jiangzheyi1234@gmail.com>
2026-04-21 23:37:29 +0800

    omx(team): auto-checkpoint worker-1 [1]

--- apps/web/app/results/[resultId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-export function generateStaticParams() {
-  return [{ resultId: "res_run_2026_0419_001" }];
-}
-
-export default function Page() {
-  redirect("/servers");
-}
