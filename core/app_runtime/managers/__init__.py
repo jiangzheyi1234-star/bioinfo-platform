bd0a911f3aad3060859c5d1f0859a3bacb48ebe4
Jiang Zheyi <jiangzheyi1234@gmail.com>
2026-06-07 00:44:36 +0800

    refactor: delegate execution runtime ops to manager

--- core/app_runtime/managers/__init__.py
+++ core/app_runtime/managers/__init__.py
@@ -2,7 +2,8 @@
 
 from core.app_runtime.managers.base import BaseRuntimeManager
 from core.app_runtime.managers.database import DatabaseManager
+from core.app_runtime.managers.execution import ExecutionManager
 from core.app_runtime.managers.tool import ToolManager
 from core.app_runtime.managers.workflow import WorkflowManager
 
-__all__ = ["BaseRuntimeManager", "DatabaseManager", "ToolManager", "WorkflowManager"]
+__all__ = ["BaseRuntimeManager", "DatabaseManager", "ExecutionManager", "ToolManager", "WorkflowManager"]
